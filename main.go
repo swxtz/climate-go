a68ba260722182624d70f9698dd14298da11b5a7
Gustavo Mendonça <alf4r6@gmail.com>
2023-07-02 05:35:55 -0300

    create shorty city verify

--- main.go
+++ main.go
@@ -10,6 +10,22 @@ import (
 func main() {
 	godotenv.Load()
 	apiKey := os.Getenv("API_KEY")
+
 	fmt.Println(apiKey)
-	// res, err := http.Get("")
+	teste := apiUrl(apiKey, "")
+	fmt.Println(teste)
+
+}
+
+func apiUrl(apiKey string, city string) string {
+
+	url := "http://api.weatherapi.com/v1/current.json?key=%s&q=%s&aqi=no"
+
+	if city != "" {
+		formatedUrl := fmt.Sprintf(url, apiKey, city)
+		return formatedUrl
+	}
+
+	formatedUrl := fmt.Sprintf(url, apiKey, "sao_paulo")
+	return formatedUrl
 }
